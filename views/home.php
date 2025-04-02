6bf2bd37d95dbfe70494743992dc8785b111ad55
Joaquim Perez <jivkim@gmail.com>
2025-04-02 21:53:25 +0200

    refactor: move calendar init js code in home view

--- views/home.php
+++ views/home.php
@@ -1,3 +1,24 @@
 <?php $this->layout('layout'); ?>
 
 <div id="calendar"></div>
+
+<script type="module">
+import { Calendar } from "https://cdn.skypack.dev/@fullcalendar/core@6.1.17";
+import dayGridPlugin from "https://cdn.skypack.dev/@fullcalendar/daygrid@6.1.17";
+import timeGridPlugin from "https://cdn.skypack.dev/@fullcalendar/timegrid@6.1.17";
+import listPlugin from "https://cdn.skypack.dev/@fullcalendar/list@6.1.17";
+import multiMonthPlugin from "https://cdn.skypack.dev/@fullcalendar/multimonth@6.1.17";
+
+document.addEventListener("DOMContentLoaded", function () {
+    const calendarEl = document.getElementById("calendar");
+    const calendar = new Calendar(calendarEl, {
+        plugins: [dayGridPlugin, timeGridPlugin, listPlugin, multiMonthPlugin],
+        headerToolbar: {
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek,multiMonthYear",
+        },
+    });
+    calendar.render();
+});
+</script>
--- views/layout.php
+++ views/layout.php
@@ -5,18 +5,6 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>merzi</title>
-    <script type="importmap">
-        {
-            "imports": {
-                "@fullcalendar/core": "https://cdn.skypack.dev/@fullcalendar/core@6.1.17",
-                "@fullcalendar/daygrid": "https://cdn.skypack.dev/@fullcalendar/daygrid@6.1.17",
-                "@fullcalendar/timegrid": "https://cdn.skypack.dev/@fullcalendar/timegrid@6.1.17",
-                "@fullcalendar/list": "https://cdn.skypack.dev/@fullcalendar/list@6.1.17",
-                "@fullcalendar/multimonth": "https://cdn.skypack.dev/@fullcalendar/multimonth@6.1.17"
-            }
-        }
-    </script>
-    <script type="module" src="app.js"></script>
     <link rel="stylesheet" href="output.css" />
 </head>
 
