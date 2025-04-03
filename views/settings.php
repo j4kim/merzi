eab2cc0c17f5b69681175a2da75429e4f5be5b71
Joaquim Perez <jivkim@gmail.com>
2025-04-03 22:34:42 +0200

    feat(settings): a button to remove calendar

--- views/settings.php
+++ views/settings.php
@@ -4,6 +4,13 @@
     <a href=".." class="hover:opacity-50">← Retour</a>
 </div>
 
+
+<script>
+    function deleteCal(btn) {
+        btn.closest('.calconfig').remove();
+    }
+</script>
+
 <form
     class="flex flex-col gap-8"
     method="POST"
@@ -12,7 +19,7 @@
         class="flex flex-col gap-2">
         Calendriers
         <?php foreach (J4kim\Merzi\Config::calendars() as $index => $cal): ?>
-            <div>
+            <div class="calconfig">
                 <div class="flex gap-2 flex-wrap">
                     <input
                         class="border border-slate-300 rounded-sm bg-slate-100 w-50 px-4 py-2"
@@ -29,7 +36,7 @@
                         required
                         value="<?= $cal->url ?>">
                 </div>
-                <div class="mt-1">
+                <div class="my-1 flex justify-between">
                     <label>
                         <input
                             type="checkbox"
@@ -38,6 +45,13 @@
                             <?= $cal->enabled ? 'checked' : '' ?>>
                         Activé
                     </label>
+                    <button
+                        type="button"
+                        class="hover:opacity-50 disabled:opacity-50"
+                        onclick="deleteCal(this)"
+                        <?= $index === 0 ? 'disabled' : ''  ?>>
+                        × Supprimer
+                    </button>
                 </div>
             </div>
         <?php endforeach; ?>
