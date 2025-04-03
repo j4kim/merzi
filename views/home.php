5a05507504b35ba4c5bc268ce8526774666bbf66
Joaquim Perez <jivkim@gmail.com>
2025-04-03 16:30:22 +0200

    feat(home.php): default to listYear view

--- views/home.php
+++ views/home.php
@@ -8,20 +8,23 @@
     } from "https://cdn.skypack.dev/@fullcalendar/core@6.1.17";
     import locale from "https://cdn.skypack.dev/@fullcalendar/core@6.1.17/locales/fr-ch";
     import dayGridPlugin from "https://cdn.skypack.dev/@fullcalendar/daygrid@6.1.17";
-    import timeGridPlugin from "https://cdn.skypack.dev/@fullcalendar/timegrid@6.1.17";
     import listPlugin from "https://cdn.skypack.dev/@fullcalendar/list@6.1.17";
     import multiMonthPlugin from "https://cdn.skypack.dev/@fullcalendar/multimonth@6.1.17";
 
     document.addEventListener("DOMContentLoaded", async function() {
         const calendarEl = document.getElementById("calendar");
         const calendar = new Calendar(calendarEl, {
-            plugins: [dayGridPlugin, timeGridPlugin, listPlugin, multiMonthPlugin],
+            plugins: [listPlugin, dayGridPlugin, multiMonthPlugin],
             locale,
             headerToolbar: {
                 left: "prev,next today",
                 center: "title",
-                right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek,multiMonthYear",
+                right: "dayGridMonth,listYear,multiMonthYear",
             },
+            initialView: 'listYear',
+            buttonText: {
+                list: 'Liste'
+            }
         });
         calendar.render();
 
