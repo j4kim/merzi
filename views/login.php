d1b1ab1aec627399b9588b1865fd27847777f4ae
Joaquim Perez <jivkim@gmail.com>
2025-04-03 21:26:11 +0200

    login, logout: codestyle and button style

--- views/login.php
+++ views/login.php
@@ -3,26 +3,23 @@
 <form
     class="flex flex-col gap-2 mt-4"
     method="POST"
-    action="login"
->
+    action="login">
     <p>
         <input
             class="border border-slate-300 rounded-sm bg-slate-100 w-50 px-4 py-2"
             name="passphrase"
             type="password"
             placeholder="mot de passe"
-            required
-        >
+            required>
     </p>
     <p>
         <button
-            class="border border-slate-300 rounded-sm bg-slate-100 w-50 px-4 py-2"
-            type="submit"
-        >
+            class="border border-slate-300 rounded-sm bg-slate-500 text-white hover:bg-slate-600 w-50 px-4 py-2"
+            type="submit">
             Connexion
         </button>
     </p>
-    <?php if(isset($error)) : ?>
+    <?php if (isset($error)) : ?>
         <p><?= $error ?></p>
     <?php endif ?>
 </form>
--- views/logout.php
+++ views/logout.php
@@ -3,13 +3,11 @@
 <form
     class="flex flex-col gap-2 mt-4"
     method="POST"
-    action="logout"
->
+    action="logout">
     <p>
         <button
-            class="border border-slate-300 rounded-sm bg-slate-100 w-50 px-4 py-2"
-            type="submit"
-        >
+            class="border border-slate-300 rounded-sm bg-slate-500 text-white hover:bg-slate-600 w-50 px-4 py-2"
+            type="submit">
             Déconnexion
         </button>
     </p>
