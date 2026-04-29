edaf2d360a933a647d1c3b14097b457091052708
Joaquim Perez <jivkim@gmail.com>
2026-04-29 15:40:21 +0200

    home.php: move js to public/home.js

---
+++ public/home.js
@@ -0,0 +1,96 @@
+const colors = [
+  "#55cc99",
+  "#cc9955",
+  "#9955cc",
+  "#5599cc",
+  "#99cc55",
+  "#cc5599",
+];
+
+document.addEventListener("DOMContentLoaded", async function () {
+  const calendarEl = document.getElementById("calendar");
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    locale: "fr-ch",
+    headerToolbar: {
+      left: "prev,next today",
+      center: "title",
+      right: "dayGridMonth,listYear,multiMonthYear",
+    },
+    initialView: "listYear",
+    buttonText: {
+      list: "Liste",
+    },
+    height: "95dvh",
+  });
+  calendar.render();
+
+  var response, json;
+
+  try {
+    response = await fetch(`${__BASE__}api/calendars`);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    json = await response.json();
+  } catch (error) {
+    return console.error("Fetching calendars failed.", error.message);
+  } finally {
+    calendarEl.classList.remove("loading");
+  }
+
+  const { calendars, showIndividual, showCommon } = json;
+
+  if (!calendars.length) {
+    return console.warn("No calendars returned from API");
+  }
+
+  calendars.forEach((cal, index) => {
+    if (showIndividual) {
+      cal.color = colors[index % colors.length];
+      calendar.addEventSource(cal);
+    }
+    cal.freeDates = new Set();
+    cal.events.forEach((event) => {
+      const start = dayjs(event.start);
+      const end = dayjs(event.end);
+      let d = start;
+      while (end.diff(d, "day")) {
+        cal.freeDates.add(d.format("YYYYMMDD"));
+        d = d.add(1, "day");
+      }
+    });
+    console.log("free dates for", cal.id, cal.freeDates);
+  });
+
+  if (showCommon) {
+    let commonDates = new Set(calendars[0].freeDates);
+    for (let index = 1; index < calendars.length; index++) {
+      commonDates = commonDates.intersection(calendars[index].freeDates);
+    }
+    const sortedDates = Array.from(commonDates).toSorted((x, y) => x - y);
+    console.log("common dates", sortedDates);
+    let d = dayjs(sortedDates[0]);
+    const last = dayjs(sortedDates[sortedDates.length - 1]);
+    const events = [];
+    let event = null;
+    while (last.diff(d, "day") > -2) {
+      if (commonDates.has(d.format("YYYYMMDD"))) {
+        if (!event) {
+          event = {
+            start: d.toDate(),
+            allDay: true,
+            title: "Dispo",
+          };
+        }
+      } else if (event) {
+        event.end = d.toDate();
+        events.push(event);
+        event = null;
+      }
+      d = d.add(1, "day");
+    }
+    calendar.addEventSource({
+      events,
+    });
+  }
+});
