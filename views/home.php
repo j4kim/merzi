4b7c6e5c1546eda3e4f63f4ce2d4a807229fd02b
Joaquim Perez <jivkim@gmail.com>
2026-04-29 16:08:16 +0200

    install fullcalendar v7-rc.2 from jsdeliver cdn

--- views/home.php
+++ views/home.php
@@ -2,26 +2,36 @@
 
 <div id="calendar" class="loading"></div>
 
-<?php $base = J4kim\Merzi\Config::base(); ?>
-
 <div class="mt-2 flex justify-between">
-    <a href="<?= $base ?>settings" class="hover:opacity-50">⚙ Réglages</a>
+    <a href="<?= J4kim\Merzi\Config::base() ?>settings" class="hover:opacity-50">⚙ Réglages</a>
 </div>
 
-<style>
-    .loading .fc-toolbar-title {
-        color: transparent;
-        background-image: url(https://media.tenor.com/yfaFcsYBgqMAAAAi/loading-gif.gif);
-        background-size: 30px;
-        background-repeat: no-repeat;
-        background-position: center;
-    }
-</style>
+<?php $this->start('scripts') ?>
+    <style>
+        .loading .fc-toolbar-title {
+            color: transparent;
+            background-image: url(https://media.tenor.com/yfaFcsYBgqMAAAAi/loading-gif.gif);
+            background-size: 30px;
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+    </style>
 
-<script src="lib//dayjs.min.js"></script>
-<script src='lib/fullcalendar/index.global.min.js'></script>
-<script src='lib/fullcalendar/fr-ch.global.min.js'></script>
-<script>
-    __BASE__ = "<?= $base ?>";
-</script>
-<script type="module" src="home.js"></script>
+    <script src="lib//dayjs.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@7.0.0-rc.2/all.global.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@7.0.0-rc.2/themes/monarch/global.js"></script>
+
+    <link href='https://cdn.jsdelivr.net/npm/fullcalendar@7.0.0-rc.2/skeleton.css' rel='stylesheet' />
+    <link href='https://cdn.jsdelivr.net/npm/fullcalendar@7.0.0-rc.2/themes/monarch/theme.css' rel='stylesheet' />
+    <link href='https://cdn.jsdelivr.net/npm/fullcalendar@7.0.0-rc.2/themes/monarch/palettes/purple.css' rel='stylesheet' />
+
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@7.0.0-rc.2/locales/fr-ch.global.js'></script>
+
+    <script>
+        __BASE__ = "<?= J4kim\Merzi\Config::base() ?>";
+    </script>
+
+    <script type="module" src="home.js"></script>
+<?php $this->stop() ?>
