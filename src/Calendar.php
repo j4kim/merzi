668bb34c006eae9ba691ed52b31b2268bb858437
Joaquim Perez <jivkim@gmail.com>
2026-04-29 13:35:06 +0200

    fix(Calendar): fix regex to clean illegal new lines in ics

--- src/Calendar.php
+++ src/Calendar.php
@@ -25,7 +25,8 @@ class Calendar
 
     public function parseEvents()
     {
-        $cleanIcs = preg_replace("/\n(?![A-Z]).*\n/", "\\n", $this->icsData);
+        // remove illegal new lines: new lines followed by other stuff than ical property (uppercase and dashes followed by : or ;)
+        $cleanIcs = preg_replace("/(\n)(?![A-Z\-]+[:;]).*\n/", "\\n", $this->icsData);
         $vcalendar = Reader::read($cleanIcs);
         $regex = Config::regex();
         foreach ($vcalendar->VEVENT as $vevent) {
